10e18e3a6a60405755f9f3421c38ae9d1c1403da
skier15 <irinagroza441@gmail.com>
2024-01-19 02:00:44 +0300

    finished LEARNING02

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJob()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+
+    
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-    }
-}
+        Resume resume = new Resume();
+        Job job1 = new Job();
+        Job job2 = new Job();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume._name = "Irina Groza";
+        
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engeneer";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        
+        job2._company = "Yandex";
+        job2._jobTitle = "Engeneer";
+        job2._startYear = 2022;
+        job2._endYear = 2023;  
+        
+        resume.DisplayResume();
+    }   
+}   
