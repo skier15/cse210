b845a2a86f794c7be1c5c9b2fea8b4e1e36ef672
skier15 <irinagroza441@gmail.com>
2024-01-20 23:47:41 +0300

    added code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,32 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number?");
-        int magicNumber = int.Parse(Console.ReadLine());
-
         Random randomGenerator = new Random();
-        int userNumber = randomGenerator.Next(1,100);
-        int guess = -1;
+        int magicNumber = randomGenerator.Next(1, 100);
 
-        while (guess != magicNumber)
+        int userNumber;
+        int counter = 0;
+        string answer = "N";
+
+        do
         {
+            do
+            {
+                Console.Write("What is your guess? ");
+                string userNumberString = Console.ReadLine();
+                userNumber = int.Parse(userNumberString);
+                counter++;
 
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
-            }
-            if (magicNumber > guess)
-            {
-                Console.WriteLine ("Higher");
-            }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else 
-            {
-                Console.WriteLine("You guessed it!");
-            }
-        }
+                if (userNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! Number of attempts: {counter}");
+                    Console.Write("Shall we play again? (Y/N) ");
+                    answer = Console.ReadLine();
+                    counter = 0;
+                }
+            } while (userNumber != magicNumber);
+
+        } while (answer == "Y");
+    }
+}
+     
+
+                           
     
-}    
-    
+
+ 
