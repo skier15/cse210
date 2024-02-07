4732e49c67c946cccbc2700a21860ae7f95944ba
skier15 <irinagroza441@gmail.com>
2024-02-07 21:14:26 +0300

    homework 4

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Student student = new Student("Brigham", "234");
+        string name = student.GetName();
+        
+        string number = student.GetName();
+        Console.WriteLine(name);
+        Console.WriteLine($"Student Number: {number}");
+    
+    
     }
 }
+public class Person
+{
+    private string _name;
+    public Person (string name)
+    {
+        _name=name;
+    }
+    
+
+
+    public string GetName()
+    {
+        return _name;
+    }
+}
+public class Student:Person
+{
+    private string _number;
+    public Student(string name,string number):base(name)
+    {
+        _number=number;
+        
+        
+    }
+    public string GetNumber()
+    {
+        return _number;
+    }
+}
+  
